e6bd367f076ee43d3cc132ed0594b8aab742e514
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-03-01 16:41:02 -0700

    Added that factorial.py
    This is a program that calculates the factorial of an N number.

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#Este programa esta hecho para calcular el programa de un numero N
+
+num = int(input("Dame un numero: "))
+factorial = 1
+
+if num < 0:
+
+  print("Sorry, el factorial no existe en numeros negativos")
+
+elif num == 0:
+
+    print("El factorial de 0 es 1")
+
+else:
+    for i in range(1,num + 1):
+        factorial = factorial*i
+    print("El factorial de ",num,"es",factorial)

9fec8f52997e9ee41e4f2f1074cf3fbef35718c5
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-03-02 11:39:01 -0600

    use of elif

---
+++ elif.py
@@ -0,0 +1,17 @@
+n=str(input("Are you rachel? " ))
+print()
+
+if (n=="yes"):
+    print("Your'e on the guest list ")
+
+elif (n=="no"):
+    m=str(input("What's your name? "))
+
+if (m=="ashley"):
+    print("Your'e on the guest list ")
+
+elif(m== "brandon"):
+
+    print("Your'e on the guest list ")
+else:
+    print("Your'e not invited")

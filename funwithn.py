08747bd3c0eaf435fb9503985f68f70d3699f9fc
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-03-23 19:36:02 -0600

    quiz #8

---
+++ funwithn.py
@@ -0,0 +1,16 @@
+a=int(input("enter a number:"))
+b=int(input("enter a second number:"))
+add=a+b
+dif=a-b
+divide=a/b
+multiply=a*b
+remainder=a%b
+print("the sum of",a," + ",b,"is equals to: ",add)
+
+print("the difference of",a," - ",b,"is equals to: ",dif)
+
+print("the division of",a," / ",b,"is equals to: ",divide)
+
+print("the product of",a," * ",b,"is equals to: ",multiply)
+
+print("the remainder of",a," % ",b,"is equals to: ",remainder)

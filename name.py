f8d717bfe0fd6ab66e9e4e6b1e4ebd044487c649
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-04-02 03:09:11 -0700

    Mastery17

---
+++ name.py
@@ -0,0 +1,7 @@
+name=str(input("Tell me your name: "))
+if (name== "Ashley"):
+    print("Congratulations Ashley it's your birthday!!")
+elif (name== "Susan"):
+    print("Hey Susan don't forget Ashley's birthday")
+else :
+    print ("I don't know you back off")

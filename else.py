14687acc55446b7879976f79c2d2141a53df4dec
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-04-02 01:32:22 -0700

    Delete else.py

--- else.py
+++
@@ -1,9 +0,0 @@
-#This is an example of how to use the conditional else
-
-n=float(input("enter your grade: "))
-
-if (n <= 6.0):
-    print ("Boo! you failed your test")
-
-else:
-    print ("OMG! congratulations you passed your test")

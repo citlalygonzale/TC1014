9565829a80da2dca9cafaaf2cdc87a0f13b8241e
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-04-01 23:32:19 -0700

    Delete comments.py

--- comments.py
+++
@@ -1,8 +0,0 @@
-#This is really simple to do the only thing you have to do
-#is to put this symbol "#" and then python would ignore this
-#you can write anything right here and this won't run
-
-print("how to")#you can also put anything around here and it won't shows
-
-print ("use comments")
- #the only thing that will show up is the printed ones lets see

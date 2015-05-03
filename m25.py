2d91c4db51e395752409a7da5c3449914e560284
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-05-03 17:00:11 -0500

    Mastery25

---
+++ m25.py
@@ -0,0 +1,14 @@
+#Creation and use of ranges in Python
+
+for a in range (10):
+    print(a)
+
+
+print()
+
+num=int(input("give me a number: "))
+print()
+
+num2=int(input("give me a number: "))
+for i in range (num,num2):
+    print(i)

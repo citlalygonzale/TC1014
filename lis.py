cbfff9a7d50e971d064f28bed4e4cb09ed43da3a
citlalygonzale <citlaligonzalez_19@hotmail.com>
2015-05-03 17:36:04 -0500

    Mastery23

---
+++ lis.py
@@ -0,0 +1,10 @@
+#Creation and use of lists in Python
+
+lists=["pink", "blue",1 , 4] #0,1,3,4
+print(lists[1:4])
+
+lists2=[]
+
+lists2=input("Give me something: ")
+
+print("The thing you gave me is: ",lists2)
